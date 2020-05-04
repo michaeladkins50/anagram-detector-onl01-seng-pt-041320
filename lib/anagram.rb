e574b518c3a3b1439db3ff90654351fc5eb0341c
Michael Adkins <mikeadkins1112@gmail.com>
2020-05-04 19:28:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,15 @@
 # Your code goes here!
 class Anagram
 
+  attr_accessor :match
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match
+    match_tester = 0
+    while 
+      
+    end
 end

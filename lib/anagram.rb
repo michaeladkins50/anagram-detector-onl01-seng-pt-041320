23ac8d116603d9aed7fd1f3dfe483057559915de
Michael Adkins <mikeadkins1112@gmail.com>
2020-05-04 19:37:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,15 @@
 # Your code goes here!
 class Anagram
 
-  attr_accessor :match
+  attr_accessor :anagram
 
   def initialize(anagram)
     @anagram = anagram
   end
 
-  def match
-    match_tester = 0
-    while
-
+  def match(words)
+    words.select do |word|
+      word.split("").sort == @anagram.split("").sort
+      end      
     end
-end
+  end
